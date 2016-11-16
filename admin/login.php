417dc4d8431d2c0a7ac022a17bcf819a8131c387
dprv1994 <dprv.1994@gmail.com>
2016-11-16 14:15:00 +0100

    login back

--- admin/login.php
+++ admin/login.php
@@ -1,9 +1,5 @@
 <?php 
 
-require_once '../inc/connect.php';
-require_once '../header.php';
-
-
 
 
 $post = [];
@@ -36,12 +32,20 @@ if (!empty($_SESSION)) {
 
 				if ($success) {
 					$_SESSION['user'] = $user;
-					];
-					header('Location: index.php')
-
+					header('Location: index.php');
+				}
+				else{
+					header('Location: login.php');
 				}
 			}
+			else{
+				$error[] = 'Le couple identifiant/mot de passe est incorrect';
+				$hasError = true;
+			}
 		}
+	}
+	else{
+		$hasError = true;
 	}
 
 
@@ -52,33 +56,40 @@ if (!empty($_SESSION)) {
  <html lang="fr">
  <head>
  <meta charset="utf-8">
- 	<title>Back</title>
-
-		<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-		<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
-
+ 	<title>Login</title>
  </head>
  <body>
 
- 	<h1 class="text-center text-info">
-		<i class="fa fa-user-circle" aria-hidden="true"> Connexion</i>
-	</h1>	
+	<?php
+		if ($hasError){
+		 		echo '<p style="color:red;">'.implode('<br>', $error).'</p>';
+			}
 
-	<main class="container text-center">
+		if($success){
+			echo '<p style="color:green;">LOGIN OK !</p>';
+		}
+		
+	
 
-		 <form method="post">
-		 	<label for="username">Nom de l'utlisateur:</label>
-		 	<input type="text" id="username" name="username">
-		 	<br><br>
 
-		 	<label for="password">Mot de passe:</label>
-		 	<input type="password" id="password" name="password">
-		 	<br><br>
+	  ?>
 
-		 	<button id="" name="" class="btn btn-info ">Connexion</button>
+
+
+ 	<h1>Connexion</h1>	
+
+	 <form method="post">
+		 <label for="username">Nom de l'utlisateur:</label>
+		 <input type="text" id="username" name="username">
+		 <br><br>
+
+		 <label for="password">Mot de passe:</label>
+		 <input type="password" id="password" name="password">
+		 <br><br>
+
+		 <button id="" name="" class="btn btn-info ">Connexion</button>
 
 		 </form>
-	</main>
  
  </body>
  </html>
