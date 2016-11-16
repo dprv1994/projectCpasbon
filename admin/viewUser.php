f0553c863a6a9bac2dbfdb6ba1e9cfc5a2971e6a
Shifu33 <appeliustheo@gmail.com>
2016-11-16 16:27:47 +0100

    update

---
+++ admin/viewUser.php
@@ -0,0 +1,35 @@
+<?php
+
+require_once '../inc/connect.php';
+require_once '../inc/session.php';
+require_once '../vendor/autoload.php';
+require_once '../inc/functions.php';
+
+if(isset($_GET['id']) && is_numeric($_GET['id'])) {
+	$query = $bdd->prepare('SELECT * FROM users WHERE id = :idUser');
+	$query->bindValue(':idUser', $_GET['id'], PDO::PARAM_INT);
+
+	if($query->execute()) {
+		$user = $query->fetch();
+	}
+	else {
+		var_dump($query->errorInfo());
+		die;
+	}
+}
+
+
+require_once 'header.php';
+?>
+		<ul>
+			<?php
+				echo '<li> <strong> Nom : </strong> '.$user['lastname'].'</li> <li> <strong> Prénom : </strong> '.$user['firstname'].'</li> <li> <strong> Email : </strong>'.$user['email'].'</li> <li> <strong> Pseudo : </strong>'.$user['username'].'</li> <li> <strong> Rôle : </strong>'.affichRole($user['role']).'</li> <li> <strong> Avatar : </strong> <img src="'.$user['avatar'].'"> </li>';
+			?>
+		</ul>
+
+		<br><br>
+		
+		<a href="addUser.php">Ajout d'utilisateur</a>
+		<a href="listUser.php">Liste des utilisateurs</a>
+	</body>
+</html>
---
+++ inc/functions.php
@@ -0,0 +1,17 @@
+<?php 
+/**
+ * Affiche Admin si la base de donnée retourne 1 pour le rôle et Editeur si la base de donéne retourne 2
+ * @param int contenu dans la base de donnée 
+ * @return retourne la valeur contenu par $affichage
+*/
+function affichRole($param) {
+	if($param == 1) {
+		$affichage = 'Admin';
+	}
+
+	if($param == 2) {
+		$affichage ='Editeur';
+	}
+
+	return $affichage;
+}
