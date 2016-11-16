72cb808395e4b92ad9d6d8ed1744f2ba2940095c
nikoumhook <nikoumhook+github@gmail.com>
2016-11-16 16:53:06 +0100

    accueil ds menu ok

--- admin/header.php
+++ admin/header.php
@@ -15,7 +15,7 @@
         <nav class="navbar navbar-light bg-faded">
           <ul class="nav navbar-nav">
             <li class="nav-item active">
-              <a class="nav-link" href="#">Accueil <span class="sr-only">(current)</span></a>
+              <a class="nav-link" href="index.php">Accueil <span class="sr-only">(current)</span></a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="/recette/add_recipe.php">Recette</a>
