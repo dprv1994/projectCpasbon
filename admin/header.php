dce90658d77c44ca1915d940c595c123af15f55b
nikoumhook <nikoumhook+github@gmail.com>
2016-11-16 13:50:21 +0100

    Ajout header+footer+boostraplocal

---
+++ admin/header.php
@@ -0,0 +1,16 @@
+<?php
+session_start();
+require_once '../inc/connect.php';
+?>
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <title>Admin Cpasbon</title>
+
+        <!-- import bootstrap Local si bug box-->
+        <link rel="stylesheet" href="../css/bootstrap.min.css">
+        <!-- import bootstrap Local si bug box-->
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    </head>
+    <body>
