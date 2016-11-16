4423a29c0ac6eea67b9106fcced0fc67efc35ac9
nikoumhook <nikoumhook+github@gmail.com>
2016-11-16 14:04:19 +0100

    suite header + creation session

--- admin/header.php
+++ admin/header.php
@@ -11,6 +11,6 @@ require_once '../inc/connect.php';
         <!-- import bootstrap Local si bug box-->
         <link rel="stylesheet" href="../css/bootstrap.min.css">
         <!-- import bootstrap Local si bug box-->
-        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <!-- <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous"> -->
     </head>
     <body>
