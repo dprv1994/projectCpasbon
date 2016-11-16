f40b2dc13f619c692061ee3b33836aeb33e3fd17
dprv1994 <dprv.1994@gmail.com>
2016-11-16 15:10:42 +0100

    index BACKKKKKKKKKKKKK

---
+++ admin/index.php
@@ -0,0 +1,46 @@
+<?php 
+
+
+ ?>
+ <!DOCTYPE html>
+ <html>
+ <head>
+ 	<title>Back</title>
+ </head>
+ <body>
+
+ 	<form method="post">
+
+ 		<label for="name">Nom Restaurant:</label>
+ 		<input type="text" id="name" name="name">
+ 		<br><br>
+
+ 		<label for="adress">Adresse:</label>
+ 		<input type="text" id="adress" name="adress">
+ 		<br><br>
+
+ 		<label for="zipcode">Code Postal:</label>
+ 		<input type="text" id="zipcode" name="zipcode">
+ 		<br><br>
+
+ 		<label for="city">Ville:</label>
+ 		<input type="text" id="city" name="city">
+ 		<br><br>
+
+ 		<label for="phone">Téléphone:</label>
+ 		<input type="num" id="phone" name="phone">
+ 		<br><br>
+
+ 		<label for="picture">Photo:</label>
+ 		<input type="file" id="picture" name="picture">
+ 		<br><br>
+ 		
+ 		<input type="submit" value="Modifier">
+
+
+ 	</form>
+
+
+ 
+ </body>
+ </html>
