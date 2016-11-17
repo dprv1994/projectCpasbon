7cce4225aa8abda43435627a9160383f095025eb
val0901 <symezac.valerie@neuf.fr>
2016-11-17 16:17:54 +0100

    recette

--- admin/list_recipes.php
+++ admin/list_recipes.php
@@ -8,22 +8,35 @@ if (!isset($is_logged)) {
     die;
 }
 
+require_once 'search.php';
 
-$query = $bdd->prepare('SELECT * FROM recipe');
-	if($query->execute()){
-		$recipes = $query->fetchAll(PDO::FETCH_ASSOC);
-	}
+
+	$query = $bdd->prepare('SELECT * FROM recipe '.$sql);
+
+if(!empty($sql)){
+	$query->bindValue(':search', '%'.$get['search'].'%');
+}
+
+if($query->execute()){
+	$recipes = $query->fetchAll(PDO::FETCH_ASSOC);
+}
+
 	else {
 		var_dump($query->errorInfo());
 		die;
 	}
 
-
 require_once 'header.php';
 ?>
         <h1>Liste des recettes :</h1>
-        <a href="add_recipe.php" class='btn btn-xs btn-info'>Ajout d'un recette</a>
+        <a href="add_recipe.php" class='btn btn-xs btn-info'>Ajout d'une recette</a>
         <br><br>
+
+        <form method="get">
+
+        <input type="text" name="search" placeholder="Rechercher">
+        <button type="submit"><i class="glyphicon glyphicon-search"></i></button>
+		</form>
 		<table class="table-striped col-lg-12">
 			<thead>
 				<tr>
@@ -37,20 +50,49 @@ require_once 'header.php';
 			</thead>
 
 			<tbody>
-				<?php foreach ($recipes as $recipe) {
-					echo '<tr>
-					<td>'.$recipe['title'].'</td>
-					<td>'.$recipe['preparation'].'</td>
-					<td>'.date('d/m/Y H:i:s', strtotime($recipe['date_creation'])).'</td>
-					<td>'.$recipe['id_autor'].'</td>
-                    <td style="min-width:20px;"><a  href="update_recipe.php?id='.$recipe['id'].'" title="Modifier recette"><i class="glyphicon glyphicon-pencil"></i> </a></td>
-                    <td style="min-width:20px;"><a href="delete_recipe.php?id='.$recipe['id'].'" title="Supprimer recette"><i class="glyphicon glyphicon-remove"></i></a></td></tr>';
-					}
-				?>
-				<!-- pour afficher date modification fichier filemtime ( string $filename ) -->
+
+				<?php if (empty($recipes)) { ?>
+						<tr>
+						<td colspan="6">on a rien trouvé</td>
+					</tr>
+				<?php }
+						else { ?>
+				
+					<?php foreach ($recipes as $recipe) { ?>
+
+						<tr>
+
+						<td>
+							<?php  if(isset($get['search']) && !empty($get['search'])){
+								echo preg_replace('/'.$get['search'].'/', '<mark>'.$get['search'].'</mark>', $recipe['title']);
+							} 
+							else { 
+								echo $recipe['title'];
+							} ?>
+						</td>
+						<td><?php  if(isset($get['search']) && !empty($get['search'])){
+								echo preg_replace('/'.$get['search'].'/', '<mark>'.$get['search'].'</mark>', $recipe['preparation']);
+							} 
+							else { 
+								echo $recipe['preparation'];
+							} ?></td>
+						<td><?php  if(isset($get['search']) && !empty($get['search'])){
+								echo preg_replace('/'.$get['search'].'/', '<mark>'.$get['search'].'</mark>', date('d/m/Y H:i', strtotime($recipe['date_creation'])));
+							} 
+							else { 
+								echo date('d/m/Y H:i', strtotime($recipe['date_creation']));
+							}?></td>
+						<td><?php echo $recipe['id_autor'];?></td>
+	                    <td style="min-width:20px;"><a  href="update_recipe.php?id=<?php $recipe['id']?>" title="Modifier recette"><i class="glyphicon glyphicon-pencil"></i> </a></td>
+	                    <td style="min-width:20px;"><a href="delete_recipe.php?id=<?php  $recipe['id']?>" title="Supprimer recette"><i class="glyphicon glyphicon-remove"></i></a></td>
+	                    </tr>
+					<?php } ?>
+					
+					<!-- pour afficher date modification fichier filemtime ( string $filename ) -->
+			<?php  }?>
 			</tbody>
 		</table>
 
 		<br><br>
-	</body>
+	</body><!-- /*preg_replace($patterns, $replacements, $string);*/ -->
 </html>
--- admin/search.php
+++ admin/search.php
@@ -1,35 +1,22 @@
 <?php
 
-require_once 'inc/connect.php';
-require_once 'inc/functions.php';
 
-// On vérifie que notre formulaire est soumis
-$get = []; // Il fait la même chose que $post.. il contient les données nettoyées
 
-$sql = ''; // On l'utilise uniquement pour la recherche
+$get = [];
+
+$sql = ''; 
 
 if(!empty($_GET)){
-	// On nettoie toujours les données reçues de l'utilisateur
 	foreach ($_GET as $key => $value) {
 		$get[$key] = trim(strip_tags($value));
 	}
 
-	// Ici on a une recherche
+	
 	if(isset($get['search']) && !empty($get['search'])){
-		$sql = 'WHERE name LIKE :search'; // La suite de la requete SQL pour la recherche
-
-		// Donnera ceci : SELECT * FROM articles WHERE name LIKE :search
+		$sql = 'WHERE title LIKE :search OR preparation LIKE :search OR date_creation LIKE :search'; 
 
 	}
 }
 
-// On concatène la requête avec la variable $sql. Cette variable est soit une chaine vide (si l'utilisateur n'a pas effectué de recherche), soit la suite de la requete SQL.
-$query = $bdd->prepare('SELECT * FROM articles '.$sql);
 
-if(!empty($sql)){
-	$query->bindValue(':search', '%'.$get['search'].'%');
-}
 
-if($query->execute()){
-	$articles = $query->fetchAll(PDO::FETCH_ASSOC);
-}
