1f92edea708abec978b942731cffdc897966083b
dprv1994 <dprv.1994@gmail.com>
2016-11-17 15:47:32 +0100

    404 errrrrrrrrrrrrrrr

--- admin/page404.php
+++ admin/page404.php
@@ -1,1 +1,15 @@
-page404.php
+<?php 
+
+
+ ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body background="../img/404.gif"alt="404">
+<h1 class="text-center text-info"> ERROR 404<h1>
+
+</body>
+</html>
+
