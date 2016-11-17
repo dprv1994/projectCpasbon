099d61b08c7cf3883c5633d8d4fcdd2c20584698
Shifu33 <appeliustheo@gmail.com>
2016-11-17 11:03:35 +0100

    span pour css

--- admin/viewUser.php
+++ admin/viewUser.php
@@ -31,6 +31,7 @@ if(isset($_POST['delete'])){
 		$delete = $bdd->prepare('DELETE FROM users WHERE id = :idUser');
 		$delete->bindValue(':idUser', $_GET['id'], PDO::PARAM_INT);
 
+
 		if($delete->execute()){
 			header('Location: listUser.php'); 
 			die;
@@ -43,7 +44,7 @@ require_once 'header.php';
 
         <ul>
 			<?php
-				echo '<li> <strong> Nom : </strong> '.$user['lastname'].'</li> <li> <strong> Prénom : </strong> '.$user['firstname'].'</li> <li> <strong> Email : </strong>'.$user['email'].'</li> <li> <strong> Pseudo : </strong>'.$user['username'].'</li> <li> <strong> Rôle : </strong>'.affichRole($user['role']).'</li> <li> <strong> Avatar : </strong> <img src="'.$user['avatar'].'"> </li>';
+				echo '<li> <strong> Nom : </strong> '.$user['lastname'].'</li> <li> <strong> Prénom : </strong> '.$user['firstname'].'</li> <li> <strong> Email : </strong>'.$user['email'].'</li> <li> <strong> Pseudo : </strong>'.$user['username'].'</li> <li> <strong> Rôle : </strong>'.affichRole($user['role']).'</li> <li><span id="img" class="imgClass>" <strong> Avatar : </strong> <img src="'.$user['avatar'].'"> </span> </li>';
 			?>
 		</ul>
 		<br>
