119f133aad3416fc0d15beecf6ef0b4d7faacefd
nikoumhook <nikoumhook+github@gmail.com>
2016-11-17 11:04:33 +0100

    deconcatenation

--- admin/updateUser.php
+++ admin/updateUser.php
@@ -9,9 +9,9 @@ if (!isset($is_logged)) {
     die;
 }
 
-if(isset($_SESSION['id']) && is_numeric($_SESSION['id'])) {
+if(isset($_SESSION['user']['id']) && is_numeric($_SESSION['user']['id'])) {
 	$query = $bdd->prepare('SELECT * FROM users WHERE id = :idUser');
-	$query->bindValue(':idUser', $_SESSION['id'], PDO::PARAM_INT);
+	$query->bindValue(':idUser', $_SESSION['user']['id'], PDO::PARAM_INT);
 
 	if($query->execute()) {
 		$users = $query->fetch();
@@ -88,22 +88,28 @@ if(!empty($_POST)) {
 
 require_once 'header.php';
 ?>
-		<ul>
-			<?php
-				echo '<li> <strong> Nom : </strong> '.$users['lastname'].'</li> <li> <strong> Prénom : </strong> '.$users['firstname'].'</li> <li> <strong> Email : </strong>'.$users['email'].'</li> <li> <strong> Pseudo : </strong>'.$users['username'].'</li> <li> <strong> Avatar : </strong> <img src="'.$users['avatar'].'"> </li>';
-			?>
-		</ul>
+<div class="col-lg-6">
+    <ul>
+        <li> <strong> Nom : </strong><?= $users['lastname'] ?></li>
+        <li> <strong> Prénom : </strong><?= $users['firstname'] ?></li>
+        <li> <strong> Email : </strong><?= $users['email'] ?></li>
+        <li> <strong> Pseudo : </strong><?= $users['username'] ?></li>
+        <li> <strong> Avatar : </strong><img src="<?= $users['avatar'] ?>"> </li>
 
-		<form method="post" enctype="multipart/form-data" class="col-lg-4 col-lg-offset-4">
-            <div class="from-group">
-                <label for="password">Nouveau mot de passe : </label><br>
-                <input class="form-control" type="password" id="password" name="password">
-            </div>
-            <div class="from-group">
-                <label for="avatar">Nouveau avatar :</label><br>
-                <input class="" type="file" id="avatar" name="avatar">
-            </div>
-            <br><br>
-			<input class="btn btn-info btn-lg center-block" type="submit" value="Mettre à jour de votre profil">
-		</form>
+    </ul>
+</div>
+<div class="col-lg-6">
+    <form method="post" enctype="multipart/form-data">
+        <div class="from-group">
+            <label for="password">Nouveau mot de passe : </label><br>
+            <input class="form-control" type="password" id="password" name="password">
+        </div>
+        <div class="from-group">
+            <label for="avatar">Nouveau avatar :</label><br>
+            <input class="" type="file" id="avatar" name="avatar">
+        </div>
+        <br><br>
+        <input class="btn btn-info btn-lg center-block" type="submit" value="Mettre à jour de votre profil">
+    </form>
+</div>
 <?php require_once 'footer.php'; ?>
