a3c33b076895415d3ea78dd3413e120f861534e9
nikoumhook <nikoumhook+github@gmail.com>
2016-11-17 10:08:15 +0100

    petite modif
    ajout de bouton sur index et grosse MAJ sur list

--- admin/listUser.php
+++ admin/listUser.php
@@ -19,7 +19,12 @@ if($query->execute()) {
 
 require_once 'header.php';
 ?>
-        <h1>Liste des utilisateurs :</h1>
+        <h1>Liste des utilisateurs :
+        <?php if ($is_logged == 'admin') {
+            echo '<a href="addUser.php"><button class="btn btn-xs btn-info" type="button">Ajout d\'utilisateur</button></a>';
+        }
+        ?>
+        </h1>
 		<table class="table-striped col-lg-12">
 			<thead>
 				<tr>
@@ -35,17 +40,16 @@ require_once 'header.php';
 			<tbody>
 				<?php foreach ($users as $user) {
 					echo '<tr><td>'.$user['lastname'].'</td><td>'.$user['firstname'].'</td><td>'.$user['email'].'</td><td>'.$user['username'].'</td><td>';
-                    echo (isset($is_logged) && $is_logged == 'admin') ? '<a href="updateUser.php?id='.$user['id'].'">Modifier l\'utilisateur</a>' : '';
+                    echo ($is_logged == 'admin') ? '<a href="updateUser.php?id='.$user['id'].'">Modifier l\'utilisateur</a>' : 'Non autorisé';
                     echo '</td><td>';
-                    echo (isset($is_logged) && $is_logged == 'admin')? '<a href="viewUser.php?id='.$user['id'].'">Voir l\'utilisateur</a>' :'';
+                    echo '<a href="viewUser.php?id='.$user['id'].'">Voir l\'utilisateur</a>';
                     echo '</td></tr>';
 				}
 				?>
 			</tbody>
 		</table>
 
-		<br><br>
-
-		<a href="addUser.php">Ajout d'utilisateur</a>
-	</body>
-</html>
+		<br><br><br>
+<?php 
+require_once 'footer.php';
+?>
