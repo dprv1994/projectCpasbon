05d41ccee8fe9b666a94472d81dd559f95ab5f21
nikoumhook <nikoumhook+github@gmail.com>
2016-11-18 11:52:25 +0100

    404 rename

---
+++ admin/404.php
@@ -0,0 +1,11 @@
+<?php header("HTTP/1.0 404 Not Found"); ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>404</title>
+</head>
+<body background="../img/404.gif" alt="404">
+<h1 class="text-center text-info"> ERROR 404<h1>
+
+</body>
+</html>
--- admin/page404.php
+++
@@ -1,5 +0,0 @@
-<?php require_once 'header.php'; ?>
-<body background="../img/404.gif" alt="404">
-<h1 class="text-center text-info"> ERROR 404<h1>
-
-<?php require_once 'footer.php'; ?>
